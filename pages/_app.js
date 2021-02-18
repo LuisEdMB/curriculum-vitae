9c8025b97d2a2cd3dad39ee61062c1bdfa51632e
Luis Eduardo <maestro_edu_10@hotmail.com>
2021-02-17 23:17:21 -0500

    Last changes ...

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../src/styles/main.css'
+import Head from 'next/head'
+
+const prefix = process.env.NODE_ENV === "production" 
+                  ? "/curriculum-vitae" : ""
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Curriculum Vitae</title>
+        <link rel="icon" type="image/x-icon" href={ prefix + "/favicon.ico" } />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )  
 }
